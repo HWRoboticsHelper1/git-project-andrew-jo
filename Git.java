32fda849db49511faebd79b2a3b297aa2538625c
Andrew Jo <ajo1@hwemail.com>
2025-09-24 12:43:12 -0700

    (GP-2.1): Program Can Initialize Repository
    Added the initializeRepo function which will create the git directory with an objects directory, index file, and HEAD file if they don't exist already, otherwise it'll tell you they already exist. Updated README file accordingly to explain the function.

--- Git.java
+++ Git.java
@@ -1,5 +1,44 @@
+import java.io.*;
+
 class Git {
     public static void main(String[] args) {
-        
+        initializeRepo();
+    }
+
+    public static void initializeRepo() {
+        boolean alreadyInitialized = true;
+        File gitRepo = new File("git");
+        if (!gitRepo.exists()) {
+            gitRepo.mkdir();
+            alreadyInitialized = false;
+        }
+        File objectRepo = new File("git/objects");
+        if (!objectRepo.exists()) {
+            objectRepo.mkdir();
+            alreadyInitialized = false;
+        }
+        File indexFile = new File("git/index");
+        if (!indexFile.exists()) {
+            try {
+                indexFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            alreadyInitialized = false;
+        }
+        File headFile = new File("git/HEAD");
+        if (!headFile.exists()) {
+            try {
+                headFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            alreadyInitialized = false;
+        }
+        if (!alreadyInitialized) {
+            System.out.println("Git Repository Created");
+        } else {
+            System.out.println("Git Repository Already Exists");
+        }
     }
 }
